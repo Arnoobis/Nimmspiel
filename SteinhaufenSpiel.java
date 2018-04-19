0269b280fa1ff705d3a46822fb6219b1e546fc60
Julia Struwe <jule@Luftikus.local>
2018-04-19 16:54:51 +0200

    bla

--- SteinhaufenSpiel.java
+++ SteinhaufenSpiel.java
@@ -1,11 +1,11 @@
-/** Klasse SteinhaufenSpiel*
-import Tools.IO.*;
+/** Klasse SteinhaufenSpiel**/
+;
 
-public class SteinhaufenSpiel{
+public class SteinhaufenSpiel {
 
 /**Attribute*/
 
-private Spieler [] spieler;
+private Spieler[] spieler;
 private Steinhaufen steinhaufen;
 private int dran;
 private int gewinner;
@@ -16,20 +16,31 @@ public static void main ( String [] args){
 
 
 
-/**Erzeugen Feld von Spielern*/
+	/**Erzeugen Feld von Spielern*/
 
-spieler= new Spieler[2];
+	spieler= new Spieler[2];
 	//Bereitstellen Datenspeicher für jeden Spieler
 	spieler[0]= new Spieler();
 	spieler[1]= new Spieler();
 
-/**Einlesen Spielernamen*/
+	/**Einlesen Spielernamen*/
 	for(int i=0; i<2;i++){
 		System.out.println( "Spieler "+(i+1));
-		spieler[i].name= IOTools.readLine("Wie ist Dein 		Name?");
-		}
+		spieler[i].name= IOTools.readLine("Wie ist Dein Name?");
+	}
+
+	public int dranSpieler(){
+		
+		dran= (dran+1)%2
+	}
+	
+	public int setGewinner(){
+		
+		gewinner= (dran+1)%2
+	}
 }
 
+}
 
 
 	
